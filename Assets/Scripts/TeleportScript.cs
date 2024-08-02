840ba602e7196025721deceb2e1eb5b891c70113
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-08-02 09:58:34 -0400

    portal done

--- Assets/Scripts/TeleportScript.cs
+++ Assets/Scripts/TeleportScript.cs
@@ -2,17 +2,18 @@ using UnityEngine;
 
 public class TeleportScript : MonoBehaviour
 {
+    public Vector3 manualOffset;
     public Transform targetTransform;
     private void OnTriggerEnter(UnityEngine.Collider other)
     {
         if (other.transform.CompareTag("Teleporter"))
         {
-            GameObject p = other.gameObject;
+            //GameObject p = other.gameObject;
             CharacterController cc = GetComponent<CharacterController>();
             cc.enabled = false;
-            Vector3 offset = p.transform.position - transform.position;
-            offset.y = 0;
-            transform.position = targetTransform.position + offset;
+            //Vector3 offset = p.transform.position - transform.position;
+            //offset.y = 0;
+            transform.position = targetTransform.position+manualOffset;
             cc.enabled = true;
         }
     }
