40968c1a64294750670471a4c33aeeb8e9095ee7
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-08-16 10:36:55 -0400

    Connected main room to side rooms

--- Assets/Scripts/BouncyBallShenanigans.cs
+++ Assets/Scripts/BouncyBallShenanigans.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class BouncyBallShenanigans : MonoBehaviour
 {
+    [SerializeField]
+    bool stayStillAtStart = true;
     Rigidbody rb;
+    bool randomBounce = true;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        if (stayStillAtStart) StartCoroutine(startTimer());
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        rb.AddForce(new Vector3(Random.Range(-.1f, .1f), Random.Range(-.1f, .1f), Random.Range(-.1f, .1f)), ForceMode.Impulse);
+        if (randomBounce)
+            rb.AddForce(new Vector3(Random.Range(-.1f, .1f), Random.Range(-.1f, .1f), Random.Range(-.1f, .1f)), ForceMode.Impulse);
+    }
+
+    IEnumerator startTimer()
+    {
+        randomBounce = false;
+        yield return new WaitForSeconds(1f);
+        randomBounce = true;
     }
 }
