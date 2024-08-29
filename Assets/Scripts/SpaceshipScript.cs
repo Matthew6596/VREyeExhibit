b76c2c50bb9f202a25faa0ae14c565ec13ea3bb5
rft0202 <rftester@email.neit.edu>
2024-08-29 18:00:35 -0400

    buttons

--- Assets/Scripts/SpaceshipScript.cs
+++ Assets/Scripts/SpaceshipScript.cs
@@ -4,50 +4,81 @@ using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class SpaceshipScript : MonoBehaviour
 {
     GameObject player;
     bool inShip = false;
-    GameObject playerMove;
-    GameObject shipMove;
     public Transform cockPit;
-    CharacterControllerDriver characterControllerDriver;
+    public int moveSpeed = 1;
+    public int turnSpeed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
-        characterControllerDriver = player.GetComponent<CharacterControllerDriver>();
-        playerMove = characterControllerDriver.locomotionProvider.GameObject();
-        shipMove = gameObject.transform.GetChild(0).gameObject.transform.GetChild(1).gameObject;
+    }
+
+    public void Update()
+    {
+        if(inShip)
+        {
+            GetComponent<TeleportPlayer>().Teleport(); //CHANGE PARENT OF PLAYER INSTEAD
+        }
     }
 
     public void PlayerEnterShip()
     {
         inShip = true;
 
-        //Movement
-        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        //Player Movement
         player.transform.GetChild(1).gameObject.transform.GetChild(1).gameObject.SetActive(false); //Disable Player Move
-        characterControllerDriver.locomotionProvider = shipMove.GetComponent<DynamicMoveProvider>();
 
         //Enable Interior
-        gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
     }
 
     public void PlayerExitShip()
     {
         inShip = false;
 
-        //Movement
-        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        //PlayerMovement
         player.transform.GetChild(1).gameObject.transform.GetChild(1).gameObject.SetActive(true); //Renable Player Move
-        characterControllerDriver.locomotionProvider = playerMove.GetComponent<DynamicMoveProvider>();
 
         //Disable Interior
-        gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
     }
 
+    public void MoveShipForward()
+    {
+        if(inShip)
+        {
+            Debug.Log("Moving Forwards");
+            gameObject.transform.position += new Vector3(0, 0, moveSpeed);
+        }
+    }
+    public void MoveShipBackward()
+    {
+        if (inShip)
+        {
+            Debug.Log("Moving Back");
+            gameObject.transform.position -= new Vector3(0, 0, moveSpeed);
+        }
+    }
+    public void TurnShipLeft()
+    {
+        if (inShip)
+        { //Rotate on Y
+            gameObject.transform.Rotate(new Vector3(0, turnSpeed, 0));
+        }
+    }
+    public void TurnShipRight()
+    {
+        if (inShip)
+        { //Rotate on Y
+            gameObject.transform.Rotate(new Vector3(0, -turnSpeed, 0));
+        }
+    }
 }
