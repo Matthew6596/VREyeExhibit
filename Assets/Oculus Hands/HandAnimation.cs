17a381136049ba6ec3a3e86ed723fd7558b46b1a
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-08-29 19:40:01 -0400

    Added animated hands to test perspective scene

--- Assets/Oculus Hands/HandAnimation.cs
+++ Assets/Oculus Hands/HandAnimation.cs
@@ -5,22 +5,49 @@ using UnityEngine.InputSystem;
 
 public class HandAnimation : MonoBehaviour
 {
-    public Animator anim;
-    public InputActionProperty gripInteraction;
-    public InputActionProperty triggerInteraction;
+    private Animator anim;
+    //public InputActionProperty gripInteraction;
+    //public InputActionProperty triggerInteraction;
+
+
+    private InputAction triggerAction,gripAction;
+    private float gripVal, triggerVal;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+
+        //---Set up input action---
+        string hand = (gameObject.name.ToLower().Contains("left")) ? "LeftHand" : "RightHand";
+
+        triggerAction = new("xr" + hand + "_triggerAnim", InputActionType.Value, "<XRController>{" + hand + "}/trigger", null, null, "Axis");
+        triggerAction.performed += Trigger;
+        triggerAction.started += Trigger;
+        triggerAction.canceled += Trigger;
+        triggerAction.Enable();
+
+        gripAction = new("xr" + hand + "_gripAnim", InputActionType.Value, "<XRController>{" + hand + "}/grip", null, null, "Axis");
+        gripAction.performed += Grip;
+        gripAction.started += Grip;
+        gripAction.canceled += Grip;
+        gripAction.Enable();
+        //---
     }
 
     // Update is called once per frame
     void Update()
     {
-        float gripValue = gripInteraction.action.ReadValue<float>();
-        float triggerValue = triggerInteraction.action.ReadValue<float>();
+        anim.SetFloat("Grip", gripVal);
+        anim.SetFloat("Trigger", triggerVal);
+    }
 
-        anim.SetFloat("Grip", gripValue);
-        anim.SetFloat("Trigger", triggerValue);
+    private void Trigger(InputAction.CallbackContext ctx)
+    {
+        triggerVal = ctx.ReadValue<float>();
+    }
+    private void Grip(InputAction.CallbackContext ctx)
+    {
+        gripVal = ctx.ReadValue<float>();
     }
 }
