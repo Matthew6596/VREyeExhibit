d7b77156523942f7cab1a44f9554204cb038b45a
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-09-19 17:35:32 -0400

    Updated checkpoint trigger logic and prefab

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     public Transform player;
-    private BoxCollider[] cs;
 
     private int q=100;
     public int queue { get => q; set { q = value; setAlpha(); } }
@@ -15,32 +14,49 @@ public class Checkpoint : MonoBehaviour
 
     public Action enterAction;
 
+    private Vector3 playerPrevPos;
+    private Plane intersectPlane;
+    public float radius=1;
+
     // Start is called before the first frame update
     void Start()
     {
         pqs = GetComponent<PlayQuickSound>();
-        cs = GetComponents<BoxCollider>();
+        playerPrevPos = transform.position;
+        intersectPlane = new Plane(transform.up, transform.position);
+        playerSide = intersectPlane.GetSide(player.position);
     }
 
     // Update is called once per frame
+    bool playerSide;
     void Update()
     {
-        bool playerin = false;
-        for(int i=0; i<cs.Length; i++)
-            if (cs[i].bounds.Contains(player.position)) { playerin = true; break; }
-        if (!playerin) return;
+        //---New ring checkpoint collision/trigger logic---
 
-
-        if (queue == 0) //yay!
+        Ray r = new(playerPrevPos, player.position - playerPrevPos); //player movement ray
+        bool s = intersectPlane.GetSide(player.position); //which side of plane is player on?
+        if (s!=playerSide) //Player switched from one side of checkpoint to other
         {
-            if(pqs!=null)pqs.Play();
-            queue = -1;
-            enterAction();
-        }
-        else if(queue!=-1)//Entered wrong checkpoint, too early!
-        {
+            playerSide = s;
+            if (intersectPlane.Raycast(r, out float e)) //Player's movement raycast intersects plane
+            {
+                if (Vector3.Distance(transform.position, r.GetPoint(e)) <= radius) //intersects inside ring radius
+                {
+                    if (queue == 0) //yay, player entered checkpoint!
+                    {
+                        if (pqs != null) pqs.Play();
+                        queue = -1;
+                        enterAction();
+                    }
+                    else if (queue != -1)//This is the wrong checkpoint, too early!
+                    {
 
+                    }
+                }
+            }
         }
+
+        playerPrevPos = player.position;
     }
 
     private void setAlpha()
